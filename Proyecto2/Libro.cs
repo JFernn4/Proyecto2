6008d98f0edd5b28dec92b64b122f09aae6d5289
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-10-25 09:19:29 -0600

    Cree la clase libro.

--- Proyecto2/Libro.cs
+++ Proyecto2/Libro.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Proyecto2
+namespace Proyecto_en_duo_
 {
     internal class Libro
     {
+        public string titulo { get; set; }
+        public string autor { get; set; }
+        public string isbn  { get; set; }
+        public string genero { get; set; }
+        public bool disponibilidad { get; set; } 
+        public Libro(string Titulo, string Autor, string ISBN, string Genero, bool Disponibilidad)
+        { 
+            titulo = Titulo;
+            autor = Autor;
+            isbn = ISBN;
+            genero = Genero;
+            disponibilidad = Disponibilidad;
+        }
+        
     }
 }
+
