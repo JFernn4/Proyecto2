800239760589b4dff37e0427bbd632fd1e800f95
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-10-25 09:22:23 -0600

    Clase libro

--- Proyecto2/Libro.cs
+++ Proyecto2/Libro.cs
@@ -5,22 +5,22 @@ using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Proyecto_en_duo_
+namespace Proyecto2
 {
     internal class Libro
     {
-        public string titulo { get; set; }
-        public string autor { get; set; }
-        public string isbn  { get; set; }
-        public string genero { get; set; }
-        public bool disponibilidad { get; set; } 
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Isbn  { get; set; }
+        public string Genero { get; set; }
+        public bool Disponibilidad { get; set; } 
         public Libro(string Titulo, string Autor, string ISBN, string Genero, bool Disponibilidad)
         { 
-            titulo = Titulo;
-            autor = Autor;
-            isbn = ISBN;
-            genero = Genero;
-            disponibilidad = Disponibilidad;
+            Titulo = Titulo;
+            Autor = Autor;
+            Isbn = ISBN;
+            Genero = Genero;
+            Disponibilidad = Disponibilidad;
         }
         
     }
