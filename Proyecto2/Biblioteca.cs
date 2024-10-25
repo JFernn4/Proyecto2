77c23eed5f136c72c7810bb606661deec4f07b67
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-10-25 09:57:07 -0600

    Agregué la busqueda secuencial.

--- Proyecto2/Biblioteca.cs
+++ Proyecto2/Biblioteca.cs
@@ -11,7 +11,26 @@ namespace Proyecto2
             static void AgregarLibro(LinkedList<Libro> listaDeLibros, Libro libro)
             {
             listaDeLibros.AddLast(libro);
-            }            
+            }     
+            static void BuscarLibro(LinkedList<Libro> listaDeLibros, Libro libro)
+            {
+            }
+        public int BusquedaSecuencial(LinkedList<Libro> listaDeLibros, string isbn)
+        {
+            int indice = 0;
+            LinkedListNode<Libro> nodoActual = listaDeLibros.First;
+            while (nodoActual != null)
+            {
+                if (nodoActual.ValueRef.Isbn == isbn)
+                {
+                    return indice;
+                }
+                nodoActual = nodoActual.Next;
+                indice++;
+            }
+            return -1;
+        }
+
     }
 }
 
